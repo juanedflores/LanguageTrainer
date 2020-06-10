0198cc40193aa2e9cd16845313d9609bcf909944
Glitch (juanslanguagetrainer) <none>
2020-06-10 20:37:29 +0000

    🌥🍽 Updated with Glitch

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const Datastore = require('nedb');
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-const doc = new GoogleSpreadsheet('1KbBHMAnh72Nam5I0V54vC3RqmYnJU3DytSW_v20akO8');
-require('dotenv').config();
+const Datastore = require("nedb");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const doc = new GoogleSpreadsheet(
+  "1KbBHMAnh72Nam5I0V54vC3RqmYnJU3DytSW_v20akO8"
+);
+require("dotenv").config();
+const fs = require("fs");
+const dir = "./public/grammar/lessons";
+
+let lastdoc = 0;
+let lastsheet = 0;
+let lastlesson = 0;
 
 // open up port and listen
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Starting server at ${port}`));
-app.use(express.static('public'));
+app.use(express.static("public"));
 // use json
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({ limit: "1mb" }));
 // load database
-const database = new Datastore('vocab.db');
-database.loadDatabase(function (err) {
-  database.find({}, function (err, docs) {
+const database = new Datastore("vocab.db");
+database.loadDatabase(function(err) {
+  database.find({}, function(err, docs) {
     // console.log(err || docs);
   });
 });
@@ -22,7 +30,9 @@ database.loadDatabase(function (err) {
 /*
  * Asynchronous function to access spreadsheets.
  */
-async function accessSpreadsheet () {
+async function accessSpreadsheet() {
+  let word1;
+  let word2;
   try {
     await doc.useServiceAccountAuth({
       client_email: process.env.CLIENT_EMAIL,
@@ -30,44 +40,92 @@ async function accessSpreadsheet () {
     });
     /// / loads document properties and worksheets
     await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+    const rows = await sheet.getRows();
+    let index = Math.floor(Math.random() * rows.length);
+    while (index == lastsheet) {
+      index = Math.floor(Math.random() * rows.length);
+    }
+    lastsheet = index;
+    await sheet.loadCells("C" + index + ":D" + index);
+    word1 = sheet.getCellByA1("C" + index).value;
+    word2 = sheet.getCellByA1("D" + index).value;
     // use API
-    await console.log(doc.title);
   } catch (err) {
     console.log(err);
   }
+  return {
+    first: word1,
+    second: word2
+  };
 }
 
 /*
  * Server POST request method.
  * Accepts the data enclosed in the body of the request message.
  */
-app.post('/api', (request, response) => {
-  console.log('got a request from client!');
+app.post("/api", (request, response) => {
+  console.log("got a request from client!");
   // get the data from client
   const data = request.body;
   // add a timestamp
   const timestamp = Date.now();
   data.timestamp = timestamp;
   // insert the data to database
-  database.insert(data, function (err, wordAdded) {
+  database.insert(data, function(err, wordAdded) {
     if (err) console.log("There's a problem with the database: ", err);
     else if (wordAdded) console.log("New word inserted in the database");
   });
   // send back to client
   response.json(data);
-  accessSpreadsheet();
 });
 
-app.get('/words', (request, response) => {
-// get a random doc from database.
-  database.count({}, function (err, count) {
+app.get("/words", (request, response) => {
+  // get a random doc from database.
+  database.count({}, function(err, count) {
     if (!err && count > 0) {
-      const skipCount = Math.floor(Math.random() * count);
-      database.find({}).skip(skipCount).limit(1).exec(function (err2, docs) {
-        if (!err2) {
-          response.send(docs[0]);
-        }
-      });
+      let skipCount = Math.floor(Math.random() * count);
+      while (skipCount == lastdoc) {
+        skipCount = Math.floor(Math.random() * count);
+      }
+      lastdoc = skipCount;
+      database
+        .find({})
+        .skip(skipCount)
+        .limit(1)
+        .exec(function(err2, docs) {
+          if (!err2) {
+            response.send(docs[0]);
+          }
+        });
     }
+  });
+});
+
+app.get("/spreads", (request, response) => {
+  accessSpreadsheet().then(val => response.send(val));
+  //console.log(values.first + " " + values.second);
+  //response.send(values);
+});
+
+app.get("/lessons", (request, response) => {
+  fs.readdir(dir, (err, files) => {
+    let index = Math.floor(Math.random() * files.length);
+    while (index == lastlesson) {
+      index = Math.floor(Math.random() * files.length);
+    }
+    lastlesson = index;
+
+    let file = files[index];
+    console.log(file);
+
+    fs.readFile("public/grammar/lessons/"+file, "utf8", function read(err, data) {
+      if (err) {
+        throw err;
+      }
+      const content = data;
+      let message = { first: content };
+      response.send(message);
+    });
   });
 });
--- public/sketch.js
+++ public/sketch.js
@@ -1,47 +1,72 @@
-let germanWord = "";
+let germanWord =
+  "Tap for new word\nSwipe left/right to change language\nSwipe up/down to toggle definition";
 let spanishWord = "";
 let englishWord = "";
+let definitionWord = "";
+let wordType = "";
+let wordText;
 let language = 0;
+let definition = 0;
 let germanGender = "000000";
-const spanishGender = 0;
+let spanishGender = 0;
+
+let msg = new SpeechSynthesisUtterance();
 
 /*
  * P5js setup function
  */
-function setup () {
+function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  wordText = createDiv('Hello');
-  wordText.style('color', '#ffffff');
-  wordText.style('font-size', '20vw');
+  wordText = createDiv("Hello");
+  wordText.attribute("lang", "de");
+  wordText.style("color", "#ffffff");
+  wordText.style("font-size", "4vw");
+  wordText.style("padding", "8vw");
   wordText.center();
 
+  ////let msg = new SpeechSynthesisUtterance();
+  let voices = window.speechSynthesis.getVoices();
+  let index = 0;
+  window.speechSynthesis.getVoices().some(function(voice) {
+    if (voice.name == "Anna") {
+      return index;
+    }
+    index += 1;
+    //console.log(voice.name, voice.default ? voice.default :'');
+  });
+  msg.voice = voices[index];
+
   gender = color(255);
-  germanGender = color(255);
+  germanGender = color(0);
 
   // set options to prevent default behaviors for swipe, pinch, etc.
   const options = {
     preventDefault: true
   };
   const hammer = new Hammer(document.body, options);
-  hammer.get('swipe').set({
+  hammer.get("swipe").set({
     direction: Hammer.DIRECTION_ALL
   });
   hammer.on("swipe", swiped);
   hammer.on("tap", getWord);
 }
 
-function swiped (event) {
-  if (event.direction == 4) {
-    console.log("right");
-    language = language + 1;
+function swiped(event) {
+  if (event.direction == 4) {
+    if (definition == 0) {
+      language = language + 1;
+    }
   } else if (event.direction == 8) {
-    console.log("up");
+    definition = definition + 1;
+    language = 0;
   } else if (event.direction == 16) {
-    console.log("down");
+    definition = definition - 1;
+    language = 0;
   } else if (event.direction == 2) {
-    console.log("left");
-    language = language - 1;
+    if (definition == 0) {
+      language = language - 1;
+    }
   }
   // wrap language back around
   if (language > 2) {
@@ -49,73 +74,109 @@ function swiped (event) {
   } else if (language < 0) {
     language = 2;
   }
+
+  if (definition > 1) {
+    definition = 0;
+  } else if (definition < 0) {
+    definition = 1;
+  }
+
+  // speak the definition if it appears on screen
+  if (definition == 1) {
+    msg.text = definitionWord;
+    speechSynthesis.speak(msg);
+  }
 }
 
-function tapped (event) {
-  console.log(event);
-}
-
-async function getWord () {
+async function getWord() {
   // Fetch to get the current database.
-  await fetch('/words', {
-    method: 'GET',
+  await fetch("/words", {
+    method: "GET",
     headers: { "Content-Type": "application/json" }
   })
     .then(response => {
       return response.json();
     })
     .then(data => {
-      console.log(data.german);
-      console.log(data.spanish);
-      console.log(data.english);
-
-      if (data.german != null) {
+      wordType = data.type;
+      if (data.german != null && data.german != germanWord) {
         germanWord = data.german;
         // determine german gender
-        const article = split(data.german, ' ')[0];
-        if (article.toLowerCase() == "der") {
+        const germanArticle = split(data.german, " ")[0];
+        if (germanArticle.toLowerCase() == "der") {
           germanGender = color(0, 0, 255);
-        } else if (article.toLowerCase() == "die") {
+        } else if (germanArticle.toLowerCase() == "die") {
           germanGender = color(255, 0, 0);
-        } else if (article.toLowerCase() == "das") {
-          germanGender = color(0, 255, 0);
+        } else if (germanArticle.toLowerCase() == "das") {
+          germanGender = color(30, 130, 0);
         } else {
-          germanGender = color(255);
+          germanGender = color(0);
         }
       } else {
-        germanWord = "null";
+        //germanWord = "null";
+        //getWord()
       }
-      if (data.spanish != null) {
+      if (data.spanish != null && data.spanish != spanishWord) {
         spanishWord = data.spanish;
+        const spanishArticle = split(data.spanish, " ")[0];
+        if (spanishArticle.toLowerCase() == "el") {
+          spanishGender = color(93, 144, 173);
+        } else if (spanishArticle.toLowerCase() == "la") {
+          spanishGender = color(179, 124, 89);
+        }
       } else {
-        spanishWord = "null";
+        //spanishWord = "null";
+        //getWord()
       }
-      if (data.english != null) {
+      if (data.english != null && data.english != englishWord) {
         englishWord = data.english;
       } else {
-        englishWord = "null";
+        //englishWord = "null";
+        //getWord()
+      }
+      if (
+        data.germanDefinition != null &&
+        data.germanDefinition != definitionWord
+      ) {
+        definitionWord = data.germanDefinition;
+      } else {
+        //definitionWord = "null";
+        //getWord()
       }
     })
     .catch(err => console.log(err));
+
+  msg.text = germanWord;
+  speechSynthesis.speak(msg);
+
+  language = 0;
+  definition = 0;
 }
 
 /*
  * P5js draw function.
  */
-function draw () {
+function draw() {
   background(0);
-  textSize(100);
-  textAlign(CENTER);
-  fill(255);
-  if (language == 0) {
-    background(germanGender);
-    wordText.html(germanWord);
-  } else if (language == 1) {
+  if (definition == 0) {
+    if (language == 0) {
+      background(germanGender);
+      wordText.html(germanWord);
+    } else if (language == 1) {
+      background(0);
+      wordText.html(englishWord);
+    } else if (language == 2) {
+      background(spanishGender);
+      wordText.html(spanishWord);
+    }
+  } else {
     background(0);
-    wordText.html(englishWord);
-  } else if (language == 2) {
-    background(0);
-    wordText.html(spanishWord);
+    wordText.html(definitionWord);
   }
   wordText.center();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  redraw();
 }
