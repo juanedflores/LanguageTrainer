1606079839947d0cd8f5a0c346e18f6af71bd880
Glitch (juanslanguagetrainer) <none>
2020-07-17 01:28:59 +0000

    tts in iOS

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,4 @@
+
 // DOM element.
 let div;
 
@@ -51,6 +52,25 @@ function setup() {
 
   getSpeech();
 }
+
+function enableAutoTTS() {
+  if (typeof window === 'undefined') {
+    return;
+  }
+  const isiOS = navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+  if (!isiOS) {
+    return;
+  }
+  const simulateSpeech = () => {
+    const lecture = new SpeechSynthesisUtterance('hello');
+    lecture.volume = 0;
+    speechSynthesis.speak(lecture);
+    document.removeEventListener('click', simulateSpeech);
+  };
+
+  document.addEventListener('click', simulateSpeech);
+}
+enableAutoTTS();
 
 /*
  * Set up text to speech.
